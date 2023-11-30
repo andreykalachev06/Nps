d31da73cd16de52ec83a40e0f4e532259baf9238
andreykalachev06 <149513589+andreykalachev06@users.noreply.github.com>
2023-11-30 22:35:44 +0300

    Add files via upload

---
+++ Nps.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nps : MonoBehaviour
+{
+    //xp nps
+    public int health = 5;
+
+    //yp nps
+    public int level = 1;
+
+    //cpeed nps
+    public float speed = 1.2f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
